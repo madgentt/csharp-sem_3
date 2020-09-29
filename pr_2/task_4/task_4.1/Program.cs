5a5c19d595dc2a6609c260b64979747608f1a15c
Artem <artemkartunchikov@mail.ru>
2020-09-29 12:04:06 +0300

    completed

---
+++ pr_2/task_4/task_4.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace pr_02_04_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // При использовании GetMachineStoreForAssembly возникают проблемы с доступом,
+            // так что здесь хранилище будет создаваться в директории пользователя, а не системы
+
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("User Prefs");
+            userWriter.Close();
+        }
+    }
+}
