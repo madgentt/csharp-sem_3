5c305a170a671e200bba981b6817ee49b87b0bc0
student <student@C251U32>
2020-10-02 11:58:38 +0300

    ???

---
+++ pr_6/Article.cs
@@ -0,0 +1,37 @@
+﻿using pr_06;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pr_6
+{
+  class Article
+  {
+    public string Name { get; set; }
+    public double Rating { get; set; }
+    public Person PersonI { get; set; }
+    public Article(string name, double rating, Person personI)
+    {
+      Name = name;
+      Rating = rating;
+      PersonI = personI;
+    }
+
+    public Article()
+    {
+      Name = "ArticleName";
+      Rating = 5.0;
+      PersonI.Name = "a";
+      PersonI.Surname = "a";
+      PersonI.BirthDate = DateTime.Now;
+
+    }
+    public override string ToString()
+    {
+      return Name + ": " + Rating + " " + PersonI.Name + " " + PersonI.Surname + " " + PersonI.BirthDate.ToString("dd/MM/yyyy");
+    }
+
+
+
+  }
+}
---
+++ pr_6/Frequency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pr_6
+{
+  public enum Frequency
+  {
+    Weekly, 
+    Monthly, 
+    Yearly
+  }
+
+
+}
